0cc11fd49211490db6b3536c429e52ef71d46a6c
Dessel Bane <desselbane999+github@gmail.com>
2026-09-13 23:29:07 +0200

    Move to pnpm devEngines and update node to 26.8.1

--- packages/configs/node/meta-updater.ts
+++ packages/configs/node/meta-updater.ts
@@ -2,7 +2,6 @@ import { existsSync } from 'node:fs'
 import { glob, readFile, writeFile } from 'node:fs/promises'
 import path from 'node:path'
 import { format, resolveConfig } from 'prettier'
-import { z } from 'zod'
 
 const PACKAGE_JSON_UPDATES = {
   type: 'module',
@@ -23,26 +22,6 @@ while (!existsSync(path.join(repoRoot, '.git'))) {
 }
 
 const rootPackageJsonPath = path.join(repoRoot, 'package.json')
-const rootPackageJson: unknown = JSON.parse(
-  await readFile(rootPackageJsonPath, 'utf8'),
-)
-
-const entrySchema = z.object({
-  name: z.string(),
-  version: z.string(),
-})
-const rootPackageJsonSchema = z.object({
-  devEngines: z.object({
-    packageManager: entrySchema,
-    runtime: entrySchema,
-  }),
-  volta: z.object({
-    node: z.string(),
-    pnpm: z.string(),
-  }),
-})
-
-const rootPackageJsonParsed = rootPackageJsonSchema.parse(rootPackageJson)
 
 const globResult = glob('**/package.json', {
   cwd: repoRoot,
@@ -64,7 +43,6 @@ for await (const entry of globResult) {
   const updatedPackageJson = {
     ...packageJsonParsed,
     ...PACKAGE_JSON_UPDATES,
-    ...rootPackageJsonParsed,
   }
   const updatedPackageJsonText = JSON.stringify(updatedPackageJson)
 
